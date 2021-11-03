bc5ef20cbb5b87f04d5c6ce203509b5d1c456dfb
Melaniem18 <73828280+Melaniem18@users.noreply.github.com>
2021-11-03 08:59:10 +0100

    cours bdd

---
+++ 01-Banks/BankAccounts_v1_tests.sql
@@ -0,0 +1,8 @@
+INSERT INTO banks
+(bank_id, bank_name, bank_balance)
+VALUES
+(102, 'Toti', 15000),
+(103, 'Tota', 1500),
+(104, 'Totu', 1500000);
+
+SELECT * FROM banks;
---
+++ 01-Banks/BankAccounts_v1_triggers.sql
@@ -0,0 +1,23 @@
+
+/*
+Evenements base de données : INSERT, UPDATE, DELETE
+Evenements base de données : BEFORE, AFTER
+*/
+USE usal3K_2021_banks;
+
+DROP TRIGGER IF EXISTS bank_insert;
+
+DELIMITER || 
+
+CREATE TRIGGER bank_insert BEFORE INSERT ON banks FOR EACH ROW 
+BEGIN
+	DECLARE balanceMin INT DEFAULT 20000;
+    SET balanceMin = 20000;
+    
+    IF balanceMin > NEW.bank_balance
+    THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'The bank has not enough money !';
+    END IF;
+END ||
+
+DELIMITER ;
